22f5620339d865e174ce298f47317590082c5c8e
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-06-15 22:46:09 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -5,27 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String x = scan.nextLine();
-        int cnt =0;
-        String[] abc = {"c=","c-","dz=","d-","lj","nj","s=","z="};        		
-        int index = 0;
         
-        while (index < x.length()) {
-            boolean tf = false;
-            for (String a : abc) {
-                if (x.startsWith(a, index)) {
-                    cnt++;
-                    index += a.length();  
-                    tf = true;
-                    break;
-                }
-            }
-            
-            if (!tf) { 
-                cnt++;
-                index++;
-            }
+     
+        String[] croatian = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        
+      
+        for (String pattern : croatian) {
+            x = x.replace(pattern, " ");
         }
         
-        System.out.println(cnt);
+ 
+        System.out.println(x.length());
     }
 }
