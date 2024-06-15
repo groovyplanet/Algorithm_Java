7e552df5fc544aaab00138b579fcbf8a3f25e596
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-06-15 18:39:31 +0900

    [level 0] Title: 세 개의 구분자, Time: 57.04 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,17 +1,6 @@
-import java.util.ArrayList;
-import java.util.*;
 class Solution {
     public String[] solution(String myStr) {
-        String[] x = myStr.split("[abc]");
-        List<String> list = new ArrayList<>();
-        for (String k : x) {
-            if (!k.isEmpty()) {
-                list.add(k);
-            }
-        }  
-        if (list.isEmpty()) {
-        	list.add("EMPTY");
-        }
-        return list.toArray(new String[0]);
+        String[] answer = {};
+        return answer;
     }
 }
