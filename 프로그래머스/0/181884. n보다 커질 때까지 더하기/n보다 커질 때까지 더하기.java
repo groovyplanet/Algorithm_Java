76c363787aee7ccde8a289648b473c16ead98721
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-06-16 23:09:00 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int solution(int[] numbers, int n) {
+		int answer = 0;
+		for(int i = 0 ; i<numbers.length; i++) {
+			if(answer<=n) {
+				answer+=numbers[i];
+			}else if(answer>n) {
+
+				break;
+			}
+		}
+
+
+		return answer;
+	}
+}
