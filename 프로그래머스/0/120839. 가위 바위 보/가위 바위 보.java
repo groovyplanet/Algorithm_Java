a2349a9f46ff37ae2e9e28d0c7b458a2e0f141ee
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-06-16 23:08:51 +0900

    [level 0] Title: 가위 바위 보, Time: 2.94 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        rsp.split("");
+        for(int i = 0 ; i<rsp.length(); i++) {
+        	char ch = rsp.charAt(i);
+        	if(ch=='2') {
+        		answer+='0';
+        	}if(ch=='0') {
+        		answer+='5';
+        		
+        	}if(ch=='5') {
+        		answer+='2';
+        	}
+        }
+        
+        return answer;
+    }
+}
