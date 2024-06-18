6fb42ade93ad43d637f96c2a8f52d4d11214ad68
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-06-18 19:19:31 +0900

    [level 0] Title: 2의 영역, Time: 1.30 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
-        int min = 1000000;
-        int max = 1;
-        List <Integer> list = new ArrayList<Integer>();
-        for(int i = 0 ; i<arr.length; i++) {
-        	if(arr[i]==2) {
-        		 min = Math.min(min, i);
-                 max = Math.max(max, i);
-        		
-        		}
-        		
-        	}
-    
-        if(min<=max) {
-        	return Arrays.copyOfRange(arr, min, max+1);
-        	//Arrays.copyOfRange 메서드는 시작 인덱스부터 끝 직전까지 출력됨
-        	//Arrays.copyOfRange = (원본배열에서 , 첫번째 인덱스 , 끝 인덱스) 뽑아서 재배열화. 
-        }else {
-        	return new int[]{-1};
-        }
- 
+        return answer;
     }
 }
