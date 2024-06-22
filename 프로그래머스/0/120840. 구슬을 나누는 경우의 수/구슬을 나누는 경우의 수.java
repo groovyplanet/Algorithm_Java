97793a80906ec717a33b7bdad3e9af4fa91772a8
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-06-22 22:08:46 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 338.50 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int balls, int share) {
-        return method(balls,share);
+        int answer = 0;
+        return answer;
     }
-    
-    public static int method(int balls, int share){
-        if(balls==share || share ==0) return 1;
-        return method((balls-1),(share-1)) + method(balls -1 , share);
-    }
- 
 }
