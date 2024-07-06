0084a33fde984abdf8b474c065dbadf62d705cd1
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-07-06 22:50:08 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,53 +1,54 @@
 class Solution {
-    public int solution(int a, int b, int c, int d) {
-        int[] arr = {a, b, c, d};
+	public int solution(int a, int b, int c, int d) {
+		int answer = 0;
+		int [] arr = new int [4];
 
-        int p = 0;
-        int q = 0;
-        int z = 0;
+		arr[0]=a;
+		arr[1]=b;
+		arr[2]=c;
+		arr[3]=d;
 
-        for (int i = 0; i < arr.length; i++) {
-            int cnt = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    cnt++;
-                }
-            }
-            if (cnt == 4) {
-                return 1111 * arr[i];
-            } else if (cnt == 3) {
-                for (int j = 0; j < arr.length; j++) {
-                    if (arr[j] != arr[i]) {
-                        q = arr[j];
-                    }
-                }
-                return (10 * arr[i] + q) * (10 * arr[i] + q);
-            } else if (cnt == 2) {
-                z++;
-                if (p == 0) {
-                    p = arr[i];
-                } else if (p != arr[i]) {
-                    q = arr[i];
-                }
-            }
-        }
+		int p = 0;
+		int q = 0;
+		int z = 0;
 
-        if (z == 4) {
-            return (p + q) * Math.abs(p - q);
-        } else if (z == 2) {
-            int r = 0, s = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] != p && arr[i] != q) {
-                    if (r == 0) {
-                        r = arr[i];
-                    } else {
-                        s = arr[i];
-                    }
-                }
-            }
-            return r * s;
-        }
+		for(int i=0; i<arr.length; i++) {
+			int cnt =0;
+			for(int j=0; j<arr.length; j++) {
+				if(arr[i]==arr[j]) {
+					p = arr[j];
+					cnt++;
+				}else {
+					q=arr[j];
+				}
+			}
+			if(cnt==4) {
+				return 1111*a;
+			}else if(cnt==3) {
+				return (10*p+q)*(10*p+q);
+			}else if(cnt==2) {
+				z++;
+			}
 
-        return Math.min(Math.min(a, b), Math.min(c, d));
-    }
+
+		}
+		if(z==4) {
+			return (p+q)*Math.abs(p-q); 
+
+		}else if(z==2) {
+			int r = 0;
+			int s = 0;
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] != p && r == 0) {
+					r = arr[i];
+				} else if (arr[i] != p) {
+					s = arr[i];
+				}
+
+
+
+			}
+		}
+		return Math.min(Math.min(a, b),Math.min(c, d));
+	}
 }
