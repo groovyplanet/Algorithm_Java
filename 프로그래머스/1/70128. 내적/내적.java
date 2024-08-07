06b8e1d072056cadb048d10a2b68fd6bd0bab9a4
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2024-08-07 21:31:58 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        int l = a.length;
+        for (int i = 0; i < l; i++) {
+            answer+=a[i]*b[i];
+        }
+
+
+        return answer;
+    }
+}
