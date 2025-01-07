3a2b8d3f9deb3a92fe6a3254753f23c87b5c9c79
Jeongwoo <73640935+groovyplanet@users.noreply.github.com>
2025-01-07 20:24:59 +0900

    [Silver III] Title: 소수 구하기, Time: 244 ms, Memory: 21088 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,40 +1,42 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
         
-        // M부터 N까지 순회하며 isPrime 판별
+        // 인덱스를 숫자로 보고 해당 숫자가 소수인지 여부를 저장할 배열
+        boolean[] isPrime = new boolean[N + 1];
+        
+        // 2부터 N까지 일단 모두 소수(true)라고 가정 (단, 0과 1은 소수가 아니므로 제외)
+        for (int i = 2; i <= N; i++) {
+            isPrime[i] = true;
+        }
+        
+        // 에라토스테네스의 체 : i가 소수라면, i의 배수들을 모두 소수가 아니라고 표시(false)
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        // M 이상 N 이하인 소수만 출력
         StringBuilder sb = new StringBuilder();
         for (int i = M; i <= N; i++) {
-            if (isPrime(i)) {
+            if (isPrime[i]) {
                 sb.append(i).append("\n");
             }
         }
         
+        // 한꺼번에 결과 출력
         System.out.print(sb);
-    }
-    
-    // 소수 판별 메서드
-    private static boolean isPrime(int num) {
-        // 1은 소수가 아니므로 바로 false
-        if(num <= 1) {
-            return false;
-        }
-        
-        // 2부터 sqrt(num)까지 나눠떨어지면 소수가 아님
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
